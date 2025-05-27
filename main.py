84356c316ec147e7f5f687502a939cf5f25a002f
Thaís Maria <104803070+thaismbarbosa@users.noreply.github.com>
2025-05-26 22:28:52 -0300

    main.py

--- main.py
+++ main.py
@@ -4,37 +4,36 @@ def main(page: ft.Page):
     # Cria o campo de texto onde o usuário digita a tarefa
     texto = ft.TextField('Digite uma tarefa:', width=300)
     
-    # Cria a coluna onde as tarefas vão ser listadas
+    # Cria a coluna para listar as tarefas
     listar_tarefa = ft.Column()
 
-    # Função que será chamada quando o botão for clicado
+    
     def adicionar_tarefas(e):
-        # Verifica se o campo de texto está vazio
         if not texto.value:
             print('Nenhuma tarefa adicionada')
         else:
             print('Adicionar tarefa!')
-            # Cria um novo texto com o conteúdo digitado
+            
             nova_tarefa = ft.Text(texto.value)
-            # Adiciona esse texto à lista de tarefas
+            
             listar_tarefa.controls.append(nova_tarefa)
-            # Atualiza a lista para aparecer na tela
+            
             listar_tarefa.update()
-            # Limpa o campo de texto
+            
             texto.value = ""
             texto.update()
 
-    # Cria o botão e associa a função de clique
+    # Cria o botão 
     btn = ft.ElevatedButton('Adicionar Tarefa', on_click=adicionar_tarefas)
 
-    # Adiciona os elementos na página
+    
     page.add(
         texto,
         btn,
         listar_tarefa
     )
 
-# Executa a aplicação
-ft.app(target=main)
+
+ft.app(main)
 
 
